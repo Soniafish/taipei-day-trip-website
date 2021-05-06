9d655285b65f029e4c65c2bb4f6f022aa76f73fc
sonia <sonia.lin@kantech.com.tw>
2021-05-06 23:58:59 +0800

    1.製作routes資料夾封包, 將def分屬不同py檔案撰寫 2.製作config.js存網址變數, 減少雲端/本機端網址切換問題

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from flask import *
 import json
 import pymysql
+# from routes.attraction import getAttractions
+import routes.attraction as attraction_api
 
 load_dotenv()
 os.environ
@@ -57,146 +59,21 @@ def thankyou():
 
 
 @app.route("/api/attractions", methods=["GET"])
-def getAttractions():
+def handleAttractions():
 	page=request.args.get("page", 0)  #預設page值為0
 	page=int(page)
 	keyword=request.args.get("keyword", "")  #預設keyword值為""
 	# print(page)
 	# print(keyword)
-	statement=""
-	p_idx = 12 * page
-	p_count = 12
-	nextPage = page+1
+	return attraction_api.getAttractions(page, keyword, cursor)
 	
-	if keyword=="":
-		cursor.execute("select count(*) from taipeiAttrations")
-		count=cursor.fetchone()
-		print(count[0])
-		if count[0] < p_idx:
-			return Response(
-				response=json.dumps({
-					"nextPage": None,
-					"data": []
-					}),
-				status=200,
-				content_type='application/json'
-			)
-		elif count[0] <  12 * (page + 1):
-			p_count = count[0] % 12
-			nextPage = None
-			print("p_count:", p_count)
-		statement=f"select id, name, category, description, address, transport, mrt, latitude, longitude, images from taipeiAttrations order by id limit {p_idx},{p_count}"
-	else:
-		cursor.execute("select count(*) from taipeiAttrations where name like '%"+keyword+"%'")
-		count=cursor.fetchone()
-		print(count[0])
-		if count[0] <  12 * (page + 1):
-			p_count = count[0] % 12
-			nextPage = None
-			print("p_count:", p_count)
-		statement ="select id, name, category, description, address, transport, mrt, latitude, longitude, images from taipeiAttrations where name like '%"+keyword+f"%' order by id limit {p_idx},{p_count}"
-	print(statement)
-	
-	result=cursor.execute(statement)
-	# print(result)
-	if result:   
-		filterData=cursor.fetchall()   #取得景點
-
-		# print(filterData)
-		data=[]
-		for item in filterData:
-			images=item[9].split(",")
-			data.append({
-				"id": item[0],
-				"name": item[1],
-				"category": item[2],
-				"description": item[3],
-				"address": item[4],
-				"transport": item[5],
-				"mrt": item[6],
-				"latitude": item[7],
-				"longitude": item[8],
-				"images": images[0: -1]
-			})
-
-			print(data[0]["images"])
-		return Response(
-				response=json.dumps({
-					"nextPage": nextPage,
-					"data": data
-					}),
-				status=200,
-				content_type='application/json'
-			)
-
-	else:
-		return Response(
-				response=json.dumps({
-					"nextPage": None,
-					"data": []
-					}),
-				status=200,
-				content_type='application/json'
-			)
-	
-	
-	return Response(
-				response=json.dumps({
-					"error": true,
-					"message": "系統錯誤"
-					}),
-				status=500,
-				content_type='application/json'
-			)
 
 
 @app.route("/api/attraction/<attractionId>", methods=["GET"])
-def getAttraction(attractionId):
+def handleAttraction(attractionId):
 	# attractionId=int(attractionId)
 	print(type(attractionId))
-	statement=f"select id, name, category, description, address, transport, mrt, latitude, longitude, images from taipeiAttrations where id={attractionId}"
-	result=cursor.execute(statement)
-	if result:
-		filterData=cursor.fetchone()
-		# print(filterData)
-		images=filterData[9].split(",")
-		data={
-			"id": filterData[0],
-			"name": filterData[1],
-			"category": filterData[2],
-			"description": filterData[3],
-			"address": filterData[4],
-			"transport": filterData[5],
-			"mrt": filterData[6],
-			"latitude": filterData[7],
-			"longitude": filterData[8],
-			"images": images[0: -1]
-		}
-		# print(data)
-		return Response(
-				response=json.dumps({"data": data}),
-				status=200,
-				content_type='application/json'
-			)
-
-	else:
-		return Response(
-				response=json.dumps({
-					"error": "true",
-					"message": "景點編號不正確"
-				}),
-				status=400,
-				content_type='application/json'
-			)
-
-	return Response(
-				response=json.dumps({
-					"error": "true",
-					"message": "系統錯誤"
-				}),
-				status=500,
-				content_type='application/json'
-			)
+	return attraction_api.getAttraction(attractionId, cursor)
 
 
 if (os.environ['localdebug']=='true'):
---
+++ routes/attraction.py
@@ -0,0 +1,139 @@
+# !/usr/bin/python2
+# coding:utf-8
+from flask import *
+import json
+import pymysql
+
+def getAttractions(page, keyword, cursor):    
+	# print(page , keyword)
+	statement=""
+	p_idx = 12 * page
+	p_count = 12
+	nextPage = page+1
+	
+	if keyword=="":
+		cursor.execute("select count(*) from taipeiAttrations")
+		count=cursor.fetchone()
+		# print(count[0])
+		if count[0] < p_idx:
+			return Response(
+				response=json.dumps({
+					"nextPage": None,
+					"data": []
+					}),
+				status=200,
+				content_type='application/json'
+			)
+		elif count[0] <  12 * (page + 1):
+			p_count = count[0] % 12
+			nextPage = None
+			# print("p_count:", p_count)
+		statement=f"select id, name, category, description, address, transport, mrt, latitude, longitude, images from taipeiAttrations order by id limit {p_idx},{p_count}"
+	else:
+		cursor.execute("select count(*) from taipeiAttrations where name like '%"+keyword+"%'")
+		count=cursor.fetchone()
+		# print(count[0])
+		if count[0] <  12 * (page + 1):
+			p_count = count[0] % 12
+			nextPage = None
+			# print("p_count:", p_count)
+		statement ="select id, name, category, description, address, transport, mrt, latitude, longitude, images from taipeiAttrations where name like '%"+keyword+f"%' order by id limit {p_idx},{p_count}"
+	# print(statement)
+	
+	result=cursor.execute(statement)
+	# print(result)
+	if result:   
+		filterData=cursor.fetchall()   #取得景點
+
+		# print(filterData)
+		data=[]
+		for item in filterData:
+			images=item[9].split(",")
+			data.append({
+				"id": item[0],
+				"name": item[1],
+				"category": item[2],
+				"description": item[3],
+				"address": item[4],
+				"transport": item[5],
+				"mrt": item[6],
+				"latitude": item[7],
+				"longitude": item[8],
+				"images": images[0: -1]
+			})
+
+			# print(data[0]["images"])
+		return Response(
+				response=json.dumps({
+					"nextPage": nextPage,
+					"data": data
+					}),
+				status=200,
+				content_type='application/json'
+			)
+
+	else:
+		return Response(
+				response=json.dumps({
+					"nextPage": None,
+					"data": []
+					}),
+				status=200,
+				content_type='application/json'
+			)
+	
+	
+	return Response(
+				response=json.dumps({
+					"error": true,
+					"message": "系統錯誤"
+					}),
+				status=500,
+				content_type='application/json'
+			)
+
+
+def getAttraction(attractionId, cursor):
+	statement=f"select id, name, category, description, address, transport, mrt, latitude, longitude, images from taipeiAttrations where id={attractionId}"
+	result=cursor.execute(statement)
+	if result:
+		filterData=cursor.fetchone()
+		# print(filterData)
+		images=filterData[9].split(",")
+		data={
+			"id": filterData[0],
+			"name": filterData[1],
+			"category": filterData[2],
+			"description": filterData[3],
+			"address": filterData[4],
+			"transport": filterData[5],
+			"mrt": filterData[6],
+			"latitude": filterData[7],
+			"longitude": filterData[8],
+			"images": images[0: -1]
+		}
+		# print(data)
+		return Response(
+				response=json.dumps({"data": data}),
+				status=200,
+				content_type='application/json'
+			)
+
+	else:
+		return Response(
+				response=json.dumps({
+					"error": "true",
+					"message": "景點編號不正確"
+				}),
+				status=400,
+				content_type='application/json'
+			)
+
+	return Response(
+				response=json.dumps({
+					"error": "true",
+					"message": "系統錯誤"
+				}),
+				status=500,
+				content_type='application/json'
+			)
